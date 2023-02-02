0090aad2f06d0435d1bbd747563e6af66ae16dc6
Byron Zhao <byronzhao139@gmail.com>
2023-02-01 19:07:17 -0500

    finished header and mobile navigation components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,18 +9,18 @@ const Header = () => {
   return (
     <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[200px] z-30 h-[100px] lg:h-[140px] flex items-center'>
       <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
-        <Link to={'/'}>
+        <Link to={'/'} className='max-w-[200px]'>
           <div className='text-[#696c6d] text-[40px] hover:text-primary transition'>BYRON</div>
         </Link>
-        <nav className='hidden lg:flex gap-x-12 font-semibold'>
-          <Link to={'/'} className='text-[#696c6d] text-[30px] hover:text-primary transition'>Home</Link>
-          <Link to={'/about'} className='text-[#696c6d] text-[30px] hover:text-primary transition'>About</Link>
-          <Link to={'/portfolio'} className='text-[#696c6d] text-[30px] hover:text-primary transition'>Portfolio</Link>
-          <Link to={'/contact'} className='text-[#696c6d] text-[30px] hover:text-primary transition'>Contact</Link>
+        <nav className='hidden xl:flex gap-x-12 font-semibold'>
+          <Link to={'/'} className='text-[#696c6d] text-3xl hover:text-primary transition'>Home</Link>
+          <Link to={'/about'} className='text-[#696c6d] text-3xl hover:text-primary transition'>About</Link>
+          <Link to={'/portfolio'} className='text-[#696c6d] text-3xl hover:text-primary transition'>Portfolio</Link>
+          <Link to={'/contact'} className='text-[#696c6d] text-3xl hover:text-primary transition'>Contact</Link>
         </nav>
+      </div>
         <Socials />
         <MobileNav />
-      </div>
     </header>
   )
 }
